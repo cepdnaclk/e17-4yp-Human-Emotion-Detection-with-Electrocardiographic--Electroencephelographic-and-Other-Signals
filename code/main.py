62c7e4ec75c6ba6f717e41568a3ddf5f2335c721
Dadoright <donabusi1@gmail.com>
2023-06-20 23:23:41 +0530

    two new parallel processes are added to the code

--- code/main.py
+++ code/main.py
@@ -1,29 +1,85 @@
 import multiprocessing
 import time
+from flask import Flask, request
+import serial
+import continuous_threading
+import os
+import datetime
+from flask_cors import CORS
+
+COM_PORT = "COM3"
+BAUD_RATE = 115200
+
+SAVE_DIR = "DATA_FILES/"
+
+subject_id = ""
+emotion = ""
+start_datetime = ""
+data_points = []
+start_time = ""
+
+start = False
+save_file = False
+
+if not os.path.exists(SAVE_DIR):
+    os.makedirs(SAVE_DIR)
+
+app = Flask(__name__)
+CORS(app)
+
+# serialPort = serial.Serial(port=COM_PORT, baudrate=BAUD_RATE, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
+# print(serialPort)
+serial_string = ""  # To hold data coming over UART
+
+processes = []
 
 
 def task1():
-    print("Task 1 started")
-    time.sleep(2)  # Simulating some time-consuming task
-    print("Task 1 finished")
+    while True:
+        print("Task 1 running")
+        time.sleep(2)
+        print("Task 1 Finished")
 
 
 def task2():
-    print("Task 2 started")
-    time.sleep(3)  # Simulating some time-consuming task
-    print("Task 2 finished")
+    while True:
+        print("Task 2 running")
+        time.sleep(3)
+        print("Task 2 Finished")
 
 
-if __name__ == "__main__":
-    process1 = multiprocessing.Process(target=task1)
-    process2 = multiprocessing.Process(target=task2)
+@app.route("/")
+def home():
+    return "ECG Serial Communication server!"
 
-    process1.start()
-    process2.start()
-    time.sleep(2.3)
-    print("Hello World")
 
-    process1.join()
-    process2.join()
+@app.route('/start', methods=['POST'])
+def start_processes():
+    global processes
+    if len(processes) == 0:
+        process1 = multiprocessing.Process(target=task1)
+        process2 = multiprocessing.Process(target=task2)
+        processes = [process1, process2]
+        for process in processes:
+            process.start()
+        return "Started two new processes."
+    else:
+        return "Processes are already running."
 
-    print("All tasks completed")
+
+@app.route('/end', methods=['POST'])
+def end_processes():
+    global processes
+    if len(processes) > 0:
+        for process in processes:
+            process.terminate()
+            # process.join()
+        # we can use this if we want to finish the process
+        processes = []
+        return "Ended the processes."
+    else:
+        return "No processes are currently running."
+
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=5000)
