20e06c02e3da1ab561dd8c204add4a1dbb3fb068
AdithyaGallage <e17091@eng.pdn.ac.lk>
2023-06-21 22:05:13 +0530

    updates main.py

--- code/main.py
+++ code/main.py
@@ -1,22 +1,17 @@
-import datetime
-import multiprocessing
-import os
-import time
-
-import serial
 from flask import Flask, request
 from flask_cors import CORS
+import multiprocessing
+import os
+import datetime
+import time
+import serial
 
 COM_PORT = "COM3"
 BAUD_RATE = 115200
 
 SAVE_DIR = "DATA_FILES/"
 
-subject_id = ""
-emotion = ""
-start_datetime = ""
-data_points = []
-start_time = ""
+processes = []  # to temporally maintain the running processes
 
 start = multiprocessing.Value('b', False)
 
@@ -26,40 +21,20 @@ if not os.path.exists(SAVE_DIR):
 app = Flask(__name__)
 CORS(app)
 
-# Uncomment following when the data collection is connected to the port
-# serialPort = serial.Serial(port=COM_PORT, baudrate=BAUD_RATE, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
-# print(serialPort)
+def ecg_collection(request_data, start, start_datetime):
 
-serial_string = ""  # To hold data coming over UART
-processes = []  # to temporally maintain the running processes
-
-
-def ecg_collection(request_data, start):
-
-    global data_points
-    # global subject_id
-    # global emotion
-    global start_datetime
-    global start_time
-    global serial_string  # I have added this
-
-    print("Task 1.1 running")
-    time.sleep(2)
-    print("Task 1.1 Finished")
-
+    data_points = []
     start_time = time.time()
-    subject_id = str(request_data['subjectId'])
-    emotion = str(request_data['emotion'])
 
-    print(subject_id)
-    time.sleep(2)
-    print(emotion)
+    subject_id = str(request_data['subjectId'])
+    emotion = str(request_data['emotion'])    
+    print("Subject_" + subject_id + " started")
+
     serialPort = serial.Serial(port=COM_PORT, baudrate=BAUD_RATE, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
     print(serialPort)
-    print("start: " + str(start.value))
+
     while start.value:
-        print("Started")
-        if serialPort.in_waiting > 0:
+        if (serialPort.in_waiting > 0):
             # Read data out of the buffer until a new line is found
             serial_string = serialPort.readline()
             elapsed_time = time.time() - start_time
@@ -70,14 +45,7 @@ def ecg_collection(request_data, start):
             except:
                 pass
 
-        start_datetime = str(datetime.datetime.now()).split('.')[0].replace(":", "_")
-
-        print("Subject_" + subject_id + " started")
-
-        return "Subject-" + subject_id + " started"
-
     print("writing to the file .....")
-    # print(subject_id)
 
     # make directory for subject id, emotion inside the SAVE_DIR
     if not os.path.exists(os.path.join(SAVE_DIR, "S-" + subject_id)):
@@ -91,20 +59,18 @@ def ecg_collection(request_data, start):
         for line in data_points:
             f.write(f"{line}\n")
 
-    data_points = []
-    serial_string = ""
     print("file saved")
 
 
 def task2():
     print("Task 2 running")
-    time.sleep(3)
+    time.sleep(5)
     print("Task 2 Finished")
 
 
 @app.route("/")
 def home():
-    return "ECG Serial Communication server!"
+    return "ECG & EEG data retriever"
 
 
 @app.route('/start', methods=['POST'])
@@ -116,11 +82,11 @@ def start_processes():
     request_data = request.json
 
     if len(processes) == 0:
-        process1 = multiprocessing.Process(target=ecg_collection, args=(request_data, start,))
+        start_datetime = str(datetime.datetime.now()).split('.')[0].replace(":", "_")
+        process1 = multiprocessing.Process(target=ecg_collection, args=(request_data, start, start_datetime,))
         process2 = multiprocessing.Process(target=task2)
         processes = [process1, process2]
         for process in processes:
-            print("start init: " + str(start.value))
             process.start()
         return "Started two new processes."
     else:
