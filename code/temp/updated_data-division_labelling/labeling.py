cb97746827cd14f5ff5b92307f69bc875acad240
Adithya Gallage <e17091@eng.pdn.ac.lk>
2023-11-08 23:11:24 +0530

    adds the modified data division and labelling scripts

---
+++ code/temp/updated_data-division_labelling/labeling.py
@@ -0,0 +1,14 @@
+import os
+import numpy as np
+
+label = [2, 1, 1, 4, 1, 4, 1, 4, 3, 4, 2, 1, 1, 4, 4, 1, 3, 1]
+repeated_labels = np.repeat(label, 60)
+output = np.tile(repeated_labels, 23)
+
+output_directory = "PCA_Labels"
+if not os.path.exists(output_directory):
+    os.makedirs(output_directory)
+# Create and open a text file for writing
+with open('./PCA_Labels/labels.txt', 'w') as file:
+    for element in output:
+        file.write(str(element) + '\n')
