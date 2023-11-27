f984b1283257a441d598b2d58ad49e052f993c5d
Adithya Gallage <e17091@eng.pdn.ac.lk>
2023-11-27 17:49:45 +0530

    Updates the codes in PCA approach

--- code/ML_MODELS/PCA/data_division.py
+++ code/ML_MODELS/PCA/data_division.py
@@ -1,9 +1,14 @@
 import os
 import csv
+import numpy as np
+
+DEVIDE_NUMBER = 1
+Standardized = True
+path = './STANDARDIZED_PCA_EEG/' if Standardized else './PCA_EEG/'
 
 for i in range (23):
     for j in range (18):
-        with open(f'./PCA_EEG/{i+1}/{j+1}/{i+1}_{j+1}_stimuli.csv', 'r') as csvfile:
+        with open(f'{path}{i+1}/{j+1}/{i+1}_{j+1}_stimuli.csv', 'r') as csvfile:
             csv_reader = csv.reader(csvfile)
             original_data_a = []
             original_data_b = []
@@ -11,16 +16,28 @@ for i in range (23):
                 original_data_a.append(row[0])
                 original_data_b.append(row[1])
 
-            selected_data_a = original_data_a[-15360:]
-            data_string_a = " ".join(selected_data_a)
-
-            output_directory = 'PCA_Column'
+            output_directory = f'PCA_Input/'
             if not os.path.exists(output_directory):
                 os.makedirs(output_directory)
-            with open('PCA_Column/PCA_a.txt', 'a') as textfile_a:
-                textfile_a.write(data_string_a + "\n")
+
+            selected_data_a = original_data_a[-15360:]
+            selected_data_a = np.array(selected_data_a)
+            selected_data_a = np.array_split(selected_data_a, DEVIDE_NUMBER)
+            final_a = []
+            for k in range (DEVIDE_NUMBER):
+                data_string_a = " ".join(selected_data_a[k])
+                final_a.append(data_string_a)
+            with open(output_directory + 'PCA_a.txt', 'a') as textfile_a:
+                for line in final_a:
+                    textfile_a.write(line + "\n")
 
             selected_data_b = original_data_b[-15360:]
-            data_string_b = " ".join(selected_data_b)
-            with open('PCA_Column/PCA_b.txt', 'a') as textfile_b:
-                textfile_b.write(data_string_b + "\n")
+            selected_data_b = np.array(selected_data_b)
+            selected_data_b = np.array_split(selected_data_b, DEVIDE_NUMBER)
+            final_b = []
+            for k in range (DEVIDE_NUMBER):
+                data_string_b = " ".join(selected_data_b[k])
+                final_b.append(data_string_b)
+            with open(output_directory + 'PCA_b.txt', 'a') as textfile_b:
+                for line in final_b:
+                    textfile_b.write(line + "\n")
--- code/ML_MODELS/PCA/pca_standardized.py
+++ code/ML_MODELS/PCA/pca_standardized.py
@@ -1,17 +1,21 @@
 import os
 import pandas as pd
 from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
 
 for i in range(23):
     for j in range(18):
         data = pd.read_csv(f'../DREAMER/EEG/{i + 1}/{j + 1}/{i + 1}_{j + 1}_stimuli.csv')
         X = data.iloc[:, :14]
 
+        scaler = StandardScaler()
+        X_scaled = scaler.fit_transform(X)
+
         pca = PCA(n_components=2)
-        X_pca = pca.fit_transform(X)
+        X_pca = pca.fit_transform(X_scaled)
 
         pca_df = pd.DataFrame(data=X_pca, columns=['PC1', 'PC2'])
-        output_directory = f'./PCA_EEG/{i + 1}/{j + 1}'
+        output_directory = f'./STANDARDIZED_PCA_EEG/{i + 1}/{j + 1}'
         if not os.path.exists(output_directory):
             os.makedirs(output_directory)
 
